246de1e5f24fa4e0c529fbf9d69dd0e46f9799e4
Tomosius <pecukevicius@gmail.com>
2023-08-02 20:23:29 +0100

    added javascript change label text based on slider

--- assets/java_script/game.js
+++ assets/java_script/game.js
@@ -1,5 +1,4 @@
-const readline = require('readline');
-
+// Making array of difficulties for game 
 const difficulties = [
   'beginner',
   'elementary',
@@ -9,81 +8,24 @@ const difficulties = [
   'master',
   'legendary'
 ];
-
-const wordLists = {
-  beginner: ['apple', 'banana', 'cat', 'dog', 'elephant', 'fish'],
-  elementary: ['house', 'book', 'school', 'tree', 'sun', 'moon'],
-  intermediate: ['computer', 'programming', 'language', 'keyboard', 'mouse'],
-  advanced: ['television', 'university', 'jupiter', 'oxygen', 'diamond'],
-  expert: ['python', 'javascript', 'algorithm', 'database', 'framework'],
-  master: ['champion', 'phenomenal', 'hypothesis', 'incredible'],
-  legendary: ['abcdefghijklmnopqrstuvwxyz', 'elephantine', 'xylophone']
+// Words list for each difficulty
+const word_lists = {
+  beginner: ['apple', 'banana', 'cat', 'dog', 'elephant', 'fish', /* add more words */],
+  elementary: ['house', 'book', 'school', 'tree', 'sun', 'moon', /* add more words */],
+  intermediate: ['computer', 'programming', 'language', 'keyboard', 'mouse', /* add more words */],
+  advanced: ['television', 'university', 'jupiter', 'oxygen', 'diamond', /* add more words */],
+  expert: ['python', 'javascript', 'algorithm', 'database', 'framework', /* add more words */],
+  master: ['champion', 'phenomenal', 'hypothesis', 'incredible', /* add more words */],
+  legendary: ['abcdefghijklmnopqrstuvwxyz', 'elephantine', 'xylophone', /* add more words */]
 };
 
-function getRandomWord(difficulty) {
-  const words = wordLists[difficulty];
-  const randomIndex = Math.floor(Math.random() * words.length);
-  return words[randomIndex];
+// Changing label "begginer" to other diffiiculties from arrray based on slider value
+function update_slider_difficulty_name() {
+  const slider = document.getElementById("difficulty");
+  const label = document.getElementById("difficulty_label");
+  // Map the slider value to the corresponding difficulty level
+  const selectedDifficulty = difficulties[parseInt(slider.value) - 1];
+  // Update the displayed difficulty label
+  label.textContent = selectedDifficulty;
 }
 
-function displayWord(word, guessedLetters) {
-  let displayedWord = '';
-  for (const char of word) {
-    if (guessedLetters.includes(char)) {
-      displayedWord += char;
-    } else {
-      displayedWord += '_';
-    }
-  }
-  return displayedWord;
-}
-
-function hangmanGame(difficulty) {
-  const word = getRandomWord(difficulty).toLowerCase();
-  let guessedLetters = [];
-  let attempts = 7;
-
-  console.log('Welcome to Hangman!');
-  console.log(`Guess the word: ${displayWord(word, guessedLetters)}`);
-
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-
-  rl.on('line', (input) => {
-    const letter = input.trim().toLowerCase();
-    if (!guessedLetters.includes(letter)) {
-      guessedLetters.push(letter);
-      if (!word.includes(letter)) {
-        attempts--;
-      }
-    }
-
-    console.log(`Attempts left: ${attempts}`);
-    console.log(`Word: ${displayWord(word, guessedLetters)}`);
-
-    if (displayWord(word, guessedLetters) === word) {
-      console.log('Congratulations! You guessed the word correctly!');
-      rl.close();
-    } else if (attempts === 0) {
-      console.log('Game Over! You ran out of attempts.');
-      rl.close();
-    }
-  });
-}
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-rl.question('Choose difficulty (1-7):\n1. Beginner\n2. Elementary\n3. Intermediate\n4. Advanced\n5. Expert\n6. Master\n7. Legendary\n', (difficulty) => {
-  difficulty = parseInt(difficulty);
-  if (difficulty >= 1 && difficulty <= 7) {
-    hangmanGame(difficulties[difficulty - 1]);
-  } else {
-    console.log('Invalid difficulty level. Please choose a number between 1 and 7.');
-    rl.close();
-  }
-});
