68ae2bc51d4018c3594f4ca4fb3f98e0a9c0cc14
Tomosius <pecukevicius@gmail.com>
2023-08-25 16:50:21 +0100

    added function guess letter

--- assets/java_script/hangman.js
+++ assets/java_script/hangman.js
@@ -2,23 +2,14 @@
 let word = ""; //word to be guessed
 let mistakeCount = 0; //mistakes per game session, max 10, then game over
 let attempt = 0; //player guessing attempt
-let hiddenWord = ""; //defining word to be guessed
-let game = -1; // this is -1, as when player starts game, it will add 1, what will make iit zero, so later it will be possible to read full game log from nested array log_player_actions
+let hiddenWord = []; //defining word to be guessed as an array
+let game = -1; // this is -1, as when player starts game, it will add 1, what will make it zero, so later it will be possible to read the full game log from the nested array log_player_actions
 let username = ""; //player username
-let logLetter = {}; // logging all guessed letter for current game
+let logLetter = {}; // logging all guessed letter for the current game
 let finalResult = ""; // win or loose
 let elapsedTimeInSeconds = ""; //timer to log player actions
-let difficulty = "beginner"; // declaring difficulty variable and setting it to lowest value for default
-let startTime = 0; //setting to zero timer when game starts
-
-
-/* Array for logging all player actions in this pattern:
-  "Difficulty",
-  "Word",
-  "Total of attempts per word",
-  ["attempt number", "Letter guessed", "wrong or Correct", "time"],
-  "Win or Loose" */
-let logPlayerActions = {};
+let difficulty = "beginner"; // declaring difficulty variable and setting it to the lowest value for default
+let startTime = 0; //setting to zero timer when the game starts
 
 //Array of difficulties
 const difficultyArray = [
@@ -42,7 +33,7 @@ const wordArray = {
   legendary: ["ltomas", "ljonas"],
 };
 
-// array of hangman image filenames to be used in function hangmanUpdatePicture
+// array of hangman image filenames to be used in the function hangmanUpdatePicture
 const hangmanImages = [
   "h1.png",
   "h2.png",
@@ -61,9 +52,9 @@ function hangmanImageUpdate(mistakeCount) {
   let imageElement = document.getElementById("image");
   imageElement.src = "/assets/images/" + hangmanImages[mistakeCount];
   console.log("mistake count:" + mistakeCount);
-};
+}
 
-// Changing label "beginner" to other difficulties from array based on slider value
+// Changing label "beginner" to other difficulties from the array based on the slider value
 function updateSliderDifficultyLabel() {
   let slider = document.getElementById("difficultyRange");
   let label = document.getElementById("difficultyLabel");
@@ -76,7 +67,7 @@ function updateSliderDifficultyLabel() {
   console.log(difficulty);
 }
 
-//function to generate new word for game and display in "_"
+//function to generate a new word for the game and display it as "_"
 function chooseWord(difficulty) {
   let wordList = wordArray[difficulty];
   let wordIndex = Math.floor(Math.random() * wordList.length);
@@ -84,26 +75,31 @@ function chooseWord(difficulty) {
   // Get the element where the hidden word will be displayed
   let hiddenWordLocation = document.getElementById('guessedWord');
   hiddenWordLocation.innerHTML = ''; // Clear previous content
-  hiddenWord = "_".repeat(word.length);
+  hiddenWord = [];
+    for (let i = 0; i < word.length; i++) {
+    hiddenWord.push("_");
+    }
+  // Initialize the hiddenWord array with underscores
   return word; // Return the chosen word
 }
 
-//function to update hidden word
-function updateHiddenWord(word) {
+//function to update the hidden word
+function updateHiddenWord(hiddenWord) {
+  let hiddenWordLocation = document.getElementById('guessedWord');
+  hiddenWordLocation.innerHTML = ''; // Clear previous content
+
   for (let i = 0; i < hiddenWord.length; i++) {
     let hiddenWordLetter = document.createElement("span");
-    hiddenWordLetter.textContent = hiddenWordLetter[i];
+    hiddenWordLetter.textContent = hiddenWord[i];
     hiddenWordLetter.className = "frame guessWordLetters";
-    hiddenWordLocation.appendChild(hiddenWordLetter); 
+    hiddenWordLocation.appendChild(hiddenWordLetter);
   }
 }
-
-
 
 // Function to create and display in HTML alphabet as separate buttons
 function createAlphabet() {
   let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  let buttonContainer = document.getElementById('gameText'); // Getting location where buttons will be placed
+  let buttonContainer = document.getElementById('gameText'); // Getting the location where buttons will be placed
   buttonContainer.innerHTML = ""; // Clear previous content
   for (let i = 0; i < alphabet.length; i++) {
     let letter = alphabet[i];
@@ -112,60 +108,57 @@ function createAlphabet() {
     button.className = "frame letterButton";
     buttonContainer.appendChild(button);
     button.addEventListener("click", function() {
-      letterGuess(letter);    // Add event listener to the button
+      console.log("Letter clicked: " + letter); // Debugging
+      letterGuess(letter, hiddenWord, word);
     });
   }
 }
 
 // Function to handle a letter guess
-function letterGuess(letter, word) {
-  for (let i = 0; i<word.length; i++) { //checking if word contains pressed letter
-    if (letter === word[i]) { //if word conntains letter, will be executed below:
-      hiddenWord = hiddenWord.slice(0, i) + letter + hiddenWord.slice(i + 1)
+function letterGuess(letter, hiddenWord, word) {
+  for (let i = 0; i < word.length; i++) {
+    if (letter.toLowerCase() === word[i].toLowerCase()) {
+      hiddenWord[i] = letter.toUpperCase(); // Update the corresponding index in the hiddenWord array
     }
   }
-updateHiddenWord(word);
+  updateHiddenWord(hiddenWord);
 }
-
-
-
-
 
 // Function to update the timer display
 function updateTimerDisplay(elapsedTimeInSeconds) {
   let timerElement = document.getElementById("timerDisplay");
   timerElement.textContent = elapsedTimeInSeconds;
 }
+
 // Function to start the timer
 function startTimer() {
   startTime = Date.now(); // Set the start time to the current time
   // Set up a timer to update the display every second (1000 milliseconds)
   setInterval(updateTimer, 1000);
 }
-// Function to calculate elapsed time and update display
+
+// Function to calculate elapsed time and update the display
 function updateTimer() {
   let currentTime = Date.now();
   let elapsedTimeInSeconds = Math.floor((currentTime - startTime) / 1000);
   updateTimerDisplay(elapsedTimeInSeconds);
 }
 
+//function to be executed when the player starts a new game
+function newGame(event) {
+  event.preventDefault(); // Disable screen update
+  if (username === "") { // Checking if there is already a username declared
+    username = document.getElementById("username").value; // If there is no username declared, do so
+  }
+  console.log(username); // Logging the username
+  chooseWord(difficulty); // Choosing a new word for the selected difficulty
 
+  // Initialize the hiddenWord array with underscores
+  hiddenWord = Array.from({ length: word.length }, () => "_");
 
-
-
-
-
-
-//function to be executed when player starts new game
-function newGame(event){
-    event.preventDefault(); // disable screen update
-    if (username == "") { //checking if there is already username declared
-      username = document.getElementById("username").value; //if there is not username declared, doing so
-    }
-    console.log(username); //logging username
-    chooseWord(difficulty); // choosing new word for selected difficulty
-    updateHiddenWord(word); //create hidden word
-    createAlphabet(); //creating alphabet
-    startTimer(); // setting current time when game starts
-
+  updateHiddenWord(hiddenWord); // Create hidden word
+  createAlphabet(); // Creating alphabet
+  startTimer(); // Setting the current time when the game starts
 }
+
+// ... (rest of your code)
