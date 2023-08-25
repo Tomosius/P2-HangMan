7c5357376d89e11a529782217ea4016be52e1ea5
Tomosius <pecukevicius@gmail.com>
2023-08-25 14:59:46 +0100

    added timer and guess letter functions

--- assets/java_script/hangman.js
+++ assets/java_script/hangman.js
@@ -7,8 +7,9 @@ let game = -1; // this is -1, as when player starts game, it will add 1, what wi
 let username = ""; //player username
 let logLetter = {}; // logging all guessed letter for current game
 let finalResult = ""; // win or loose
-let timer = ""; //timer to log player actions
+let elapsedTimeInSeconds = ""; //timer to log player actions
 let difficulty = "beginner"; // declaring difficulty variable and setting it to lowest value for default
+let startTime = 0; //setting to zero timer when game starts
 
 
 /* Array for logging all player actions in this pattern:
@@ -83,34 +84,70 @@ function chooseWord(difficulty) {
   // Get the element where the hidden word will be displayed
   let hiddenWordLocation = document.getElementById('guessedWord');
   hiddenWordLocation.innerHTML = ''; // Clear previous content
-  for (let i = 0; i < word.length; i++) {
-    let hiddenWordLetter = document.createElement("span");
-    hiddenWordLetter.textContent = "_";
-    hiddenWordLetter.className = "frame guessWordLetters";
-    hiddenWordLocation.appendChild(hiddenWordLetter); 
-  }
+  hiddenWord = "_".repeat(word.length);
   return word; // Return the chosen word
 }
 
-
-//function to remove instructions section from screen
-function clearGameText() {
-  document.getElementById('gameText').innerHTML = "";
+//function to update hidden word
+function updateHiddenWord(word) {
+  for (let i = 0; i < hiddenWord.length; i++) {
+    let hiddenWordLetter = document.createElement("span");
+    hiddenWordLetter.textContent = hiddenWordLetter[i];
+    hiddenWordLetter.className = "frame guessWordLetters";
+    hiddenWordLocation.appendChild(hiddenWordLetter); 
+  }
 }
 
-//function to create and display in HTML alphabet as separate buttons
+
+
+// Function to create and display in HTML alphabet as separate buttons
 function createAlphabet() {
   let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  let buttonContainer = document.getElementById('gameText'); // getting location where buttons will be placed
+  let buttonContainer = document.getElementById('gameText'); // Getting location where buttons will be placed
+  buttonContainer.innerHTML = ""; // Clear previous content
   for (let i = 0; i < alphabet.length; i++) {
     let letter = alphabet[i];
     let button = document.createElement("button");
     button.textContent = letter;
     button.className = "frame letterButton";
     buttonContainer.appendChild(button);
+    button.addEventListener("click", function() {
+      letterGuess(letter);    // Add event listener to the button
+    });
   }
 }
 
+// Function to handle a letter guess
+function letterGuess(letter, word) {
+  for (let i = 0; i<word.length; i++) { //checking if word contains pressed letter
+    if (letter === word[i]) { //if word conntains letter, will be executed below:
+      hiddenWord = hiddenWord.slice(0, i) + letter + hiddenWord.slice(i + 1)
+    }
+  }
+updateHiddenWord(word);
+}
+
+
+
+
+
+// Function to update the timer display
+function updateTimerDisplay(elapsedTimeInSeconds) {
+  let timerElement = document.getElementById("timerDisplay");
+  timerElement.textContent = elapsedTimeInSeconds;
+}
+// Function to start the timer
+function startTimer() {
+  startTime = Date.now(); // Set the start time to the current time
+  // Set up a timer to update the display every second (1000 milliseconds)
+  setInterval(updateTimer, 1000);
+}
+// Function to calculate elapsed time and update display
+function updateTimer() {
+  let currentTime = Date.now();
+  let elapsedTimeInSeconds = Math.floor((currentTime - startTime) / 1000);
+  updateTimerDisplay(elapsedTimeInSeconds);
+}
 
 
 
@@ -126,10 +163,9 @@ function newGame(event){
       username = document.getElementById("username").value; //if there is not username declared, doing so
     }
     console.log(username); //logging username
-
     chooseWord(difficulty); // choosing new word for selected difficulty
+    updateHiddenWord(word); //create hidden word
+    createAlphabet(); //creating alphabet
+    startTimer(); // setting current time when game starts
 
-    clearGameText() //clearing text on screen so it is blank for future use
-
-    createAlphabet() //creating alphabet
 }
